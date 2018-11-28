9cb21e28b5fc7f95fe75478fcb4d166af6661a48
gurjit03 <gurjitmehta@gmail.com>
2018-11-28 17:33:40 +0530

    created folder for handling static files

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-type", "text-html");
-  res.setHeader("X-custom", "hello");
+  res.setHeader("Content-type", "text/html");
+  // res.setHeader("X-custom", "hello");
+  res.send("<title>THIS IS CURRENTLY THE INDEX PAGE");
 });
 
 app.get("/hello", (req, res) => {
   res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`APP LISTENING ON PORT : ${PORT}`);
 });
