07a6a9e31dc07a018e95f0d56a8d4793c2a96fa9
gurjit03 <gurjitmehta@gmail.com>
2019-01-02 01:46:31 +0530

    added express-handle bars package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const hbs = require("express-hbs");
 const passport = require("passport");
 const path = require("path");
 
@@ -6,11 +7,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.engine(
+  "hbs",
+  hbs.express4({
+    partialsDir: path.join(__dirname, "/views/partials"),
+    defaultLayout: path.resolve(__dirname, "/views/layout.hbs")
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.setHeader("Content-type", "text/html");
   // res.setHeader("X-custom", "hello");
-  res.send("<title>THIS IS CURRENTLY THE INDEX PAGE");
+  res.render("index.hbs", {});
+  // res.send("<title>THIS IS CURRENTLY THE INDEX PAGE</title>");
 });
 
 app.get("/hello", (req, res) => {
