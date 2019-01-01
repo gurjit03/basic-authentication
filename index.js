56eecccbc0dfc0f8d81a92ddde27552b1c757c99
gurjit03 <gurjitmehta@gmail.com>
2019-01-02 02:18:44 +0530

    index page added -- fixes #3

--- index.js
+++ index.js
@@ -7,20 +7,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "hbs");
 app.engine(
   "hbs",
   hbs.express4({
     partialsDir: path.join(__dirname, "/views/partials"),
-    defaultLayout: path.resolve(__dirname, "/views/layout.hbs")
+    layoutsDir: path.join(__dirname, "/views/layouts"),
+    defaultLayout: path.join(path.resolve(), "/views/layouts/default.hbs")
   })
 );
-app.set("view engine", "hbs");
+
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.setHeader("Content-type", "text/html");
   // res.setHeader("X-custom", "hello");
-  res.render("index.hbs", {});
+  res.render("index", {
+    userName: "gurjit",
+    layout: "default"
+  });
   // res.send("<title>THIS IS CURRENTLY THE INDEX PAGE</title>");
 });
 
